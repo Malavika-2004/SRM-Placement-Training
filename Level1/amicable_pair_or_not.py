82c6693466ef5a87a7ab542ec29b1b4128dcbe10
MALAVIKA JAYAKUMAR <malavikajayakumarmvk@gmail.com>
2026-09-02 21:53:01 +0530

    Create amicable_pair_or_not.py

---
+++ Level1/amicable_pair_or_not.py
@@ -0,0 +1,13 @@
+a,b=map(int,input().split())
+s1=0
+s2=0
+for i in range(1,a):
+    if a%i==0:
+        s1=s1+i
+for i in range(1,b):
+    if b%i==0:
+        s2=s2+i
+if s1==b and s2==a:
+    print("Amicable Pair")
+else:
+    print("Not a Amicable Pair")                    
