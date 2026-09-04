cf464cc9360d4469f69658c96f9a7ff65808662f
MALAVIKA JAYAKUMAR <malavikajayakumarmvk@gmail.com>
2026-09-04 20:49:43 +0530

    Create alternating_binary_pattern.py

---
+++ Patterns/number_patterns/alternating_binary_pattern.py
@@ -0,0 +1,8 @@
+n=int(input())
+for row in range(1,n+1):
+    for col in range(1,n+1):
+        if (row+col)%2==0:
+            print("1",end="")
+        else:
+            print("0",end="")    
+    print()        
