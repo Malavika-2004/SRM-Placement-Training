4b36a9db82f52a51352e243388aa42091af2ceba
MALAVIKA JAYAKUMAR <malavikajayakumarmvk@gmail.com>
2026-09-08 09:48:08 +0530

    Create checking_alphabets.py

---
+++ Fundamentals_of_Programming/checking_alphabets.py
@@ -0,0 +1,7 @@
+a = input()
+if a in "AEIOUaeiou":
+    print("Vowel")
+elif a in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz":
+    print("Consonant")
+else:
+    print("Not an alphabet")
