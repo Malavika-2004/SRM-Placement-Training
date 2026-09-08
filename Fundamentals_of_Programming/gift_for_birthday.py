4105199b5b1e9da74b1120485a2ef6a2838592b6
MALAVIKA JAYAKUMAR <malavikajayakumarmvk@gmail.com>
2026-09-08 09:50:53 +0530

    Create gift_for_birthday.py

---
+++ Fundamentals_of_Programming/gift_for_birthday.py
@@ -0,0 +1,9 @@
+n=int(input())
+if n%400==0:
+    print(f"{n} is a leap year")
+elif n%100==0:
+    print(f"{n} is not a leap year")
+elif n%4==0:
+    print(f"{n} is a leap year")
+else:
+    print(f"{n} is not a leap year")
